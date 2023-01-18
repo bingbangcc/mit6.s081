35a8c7f44cd53df7e8e8c5204cb7aa3eac94653e
bingbong <1633586528@qq.com>
2023-01-18 14:08:32 +0800

    modify

--- user/user.h
+++ user/user.h
@@ -24,6 +24,10 @@ char* sbrk(int);
 int sleep(int);
 int uptime(void);
 
+void sigalarm(int ticks, void(*handler)());
+void sigreturn(void);
+
+
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
